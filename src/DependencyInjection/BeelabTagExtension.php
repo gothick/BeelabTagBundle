992ee20a4fc66391590176765e70d5d44e02ef6d
Matt Gibson <gothick@gothick.org.uk>
2026-02-09 16:46:41 +0000

    Simple newline between assignment sets

--- src/DependencyInjection/BeelabTagExtension.php
+++ src/DependencyInjection/BeelabTagExtension.php
@@ -20,6 +20,7 @@ final class BeelabTagExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
         $container->setParameter('beelab_tag.tag_class', $config['tag_class']);
         $container->setParameter('beelab_tag.purge', $config['purge']);
+
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.xml');
     }
